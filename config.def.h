3238af85519e2493a27e0096185c7e215b48f85a
Centei <115051114+Centei@users.noreply.github.com>
2023-08-03 20:06:26 -0700

    personal tweaks/patch files and auto restart script

--- config.def.h
+++ config.def.h
@@ -1,47 +1,56 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of bar */
-static const unsigned int wborderpx = 1;	/* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int borderpx  = 5;        /* border pixel of bar */
+static const unsigned int wborderpx = 2;	/* border pixel of windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 2;        /* 2 is the default spacing around the bar's font */
+static const int user_bh            = 8;        /* 2 is the default spacing around the bar's font */
 static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_borderbar[]   = "#ff0000";
+static const int sidepad            = 12;       /* horizontal padding of bar */
+static const char *fonts[]          = { "SauceCodeProNerdFont:size=12:antialias=true:autohint=true" };
+static const char dmenufont[]       = "SauceCodeProNerdFont:size=12:antialias=true:autohint=true";
+static const char nord1[]           = "#3b4252"; //status background color
+static const char nord0[]           = "#2e3440"; //inactive window border color
+static const char nord4[]           = "#d8dee9"; //font color 
+static const char nord6[]           = "#eceff4"; //current tag und current window font color
+static const char nord10[]          = "#4c566a"; //top bar
+static const char col_black[]       = "#000000";
+static const char col_red[]         = "#ff0000";
+static const char col_yellow[]      = "#ffff00";
+static const char col_white[]       = "#ffffff";
+static const char col_borderbar[]   = "#3b4252";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { nord4, nord1, nord0 },
+	[SchemeSel]  = { nord6, nord10,  nord10 },
+	/*
+	[SchemeWarn] =	 { col_black, col_yellow, col_red },
+	[SchemeUrgent]=	 { col_white, col_red,    col_red },
+	*/
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* make new color tagset before adding new tags otherwise dwm will not load properly  */
+static const char *tags[] = { "", "󰈹", "󰙯", "󰨞", "", "", "󰢬" };
 
 static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
 static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
 static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
-static const char *tagsel[][2] = {
-	{ "#ffffff", "#ff0000" },
-	{ "#ffffff", "#ff7f00" },
-	{ "#000000", "#ffff00" },
-	{ "#000000", "#00ff00" },
-	{ "#ffffff", "#0000ff" },
-	{ "#ffffff", "#4b0082" },
-	{ "#ffffff", "#9400d3" },
-	{ "#000000", "#ffffff" },
-	{ "#ffffff", "#000000" },
+static const char *tagsel[][2][2] = {
+	/*      norm                      sel       */
+	/*  fg          bg           fg          bg  */
+	{ { "#88c0d0", nord1 }, { "#88c0d0", nord1 } },
+	{ { "#d08770", nord1 }, { "#d08770", nord1 } },
+	{ { "#ebcb8b", nord1 }, { "#ebcb8b", nord1 } },
+	{ { "#8fbcbb", nord1 }, { "#8fbcbb", nord1 } },
+	{ { "#bf616a", nord1 }, { "#bf616a", nord1 } },
+	{ { "#b48ead", nord1 }, { "#b48ead", nord1 } },
+	{ { "#a3be8c", nord1 }, { "#a3be8c", nord1 } },
 };
 
 static const Rule rules[] = {
@@ -49,13 +58,20 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	/* class      instance    title       tags mask     switchtotag     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            0,              1,           -1 },
+	{ "firefox",  "Navigator",NULL,       1 << 1,       1,              0,           -1 },
+	{ "discord",  NULL,       NULL,       1 << 2,       1,              0,            1 },
+	{ "VSCodium", NULL,       NULL,       1 << 3,       1,              0,           -1 },
+	{ "st-256color",NULL,     NULL,       1 << 4,       1,              0,           -1 },
+	{ "obsidian", NULL,       NULL,       1 << 5,       1,              0,            1 },
+	{ "KeePassXC",NULL,       NULL,       1 << 6,       1,              0,            1 },
+	{ "firefox",  "Places",   "Library",  0,            0,              1,           -1 },
+	{ "firefox",  "Browser",  NULL,       0,            0,              1,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.65; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -80,8 +96,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", nord1, "-nf", nord4, "-sb", nord10, "-sf", nord6, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *quitcmd[] = { "killall", "-p", "dwm", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -108,8 +125,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_Right,  viewnext,       {0} },
-	{ MODKEY,                       XK_Left,   viewprev,       {0} },
+	{ Mod4Mask,                     XK_Tab,    viewnext,       {0} },
+	{ Mod4Mask|ShiftMask,           XK_Tab,    viewprev,       {0} },
 	{ MODKEY|ShiftMask,             XK_Right,  tagtonext,      {0} },
 	{ MODKEY|ShiftMask,             XK_Left,   tagtoprev,      {0} },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
@@ -125,6 +142,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,			XK_q,      spawn,	   {.v = quitcmd } } 	
 };
 
 /* button definitions */
